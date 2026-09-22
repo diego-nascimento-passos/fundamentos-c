0ac5ff5fdd584cc5d3be676ca1d1969e723bba2d
Diego Passos <diegopassos603@gmail.com>
2026-09-22 11:55:42 -0300

    Exercício

---
+++ 05-funcoes/01-mostrarMensagem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    // Definição da função 'mostrarMensagem'
+    // void significa que a função executa uma ação, mas não retorna nenhum valor numérico ou caractere
+    void mostrarMensagem(){
+        printf ("Olá, Diego!\nVocê está aprendendo funções em C.");
+    }
+int main(){
+
+    // Chama (executa) a função que está lá em cima
+    mostrarMensagem();
+
+    return 0;
+}
